6b3b52350222a7124a3f442cdb0bc38f46a8281d
Andres Bores <andresmbores@gmail.com>
2020-09-07 00:51:30 -0300

    Skip send msg to sender.

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -9,33 +9,33 @@ const { TABLE_NAME } = process.env;
 
 exports.handler = async event => {
   let connectionData;
-  
+
   try {
     connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
   } catch (e) {
     return { statusCode: 500, body: e.stack };
   }
-  
+
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
     apiVersion: '2018-11-29',
     endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
   });
-  
+
   const postData = JSON.parse(event.body).data;
-  
-  const postCalls = connectionData.Items.map(async ({ connectionId }) => {
-    try {
-      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
-    } catch (e) {
-      if (e.statusCode === 410) {
-        console.log(`Found stale connection, deleting ${connectionId}`);
-        await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
-      } else {
-        throw e;
+  const postCalls = connectionData.Items.filter(({ connectionId }) => connectionId !== event.requestContext.connectionId)
+    .map(async ({ connectionId }) => {
+      try {
+        await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: JSON.stringify(postData) }).promise();
+      } catch (e) {
+        if (e.statusCode === 410) {
+          console.log(`Found stale connection, deleting ${connectionId}`);
+          await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+        } else {
+          throw e;
+        }
       }
-    }
-  });
-  
+    });
+
   try {
     await Promise.all(postCalls);
   } catch (e) {
